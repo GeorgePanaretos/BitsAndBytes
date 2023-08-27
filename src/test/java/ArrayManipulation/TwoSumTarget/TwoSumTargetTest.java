17a8a87c0b924f5529c53392781a63512850ac3c
George Panaretos <georgepanaretos.cac@gmail.com>
2023-08-27 23:17:01 +0200

    Two Sum Target Test

---
+++ src/test/java/ArrayManipulation/TwoSumTarget/TwoSumTargetTest.java
@@ -0,0 +1,12 @@
+package ArrayManipulation.TwoSumTarget;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTargetTest {
+
+    @Test
+    void hasTwoSum() {
+    }
+}
