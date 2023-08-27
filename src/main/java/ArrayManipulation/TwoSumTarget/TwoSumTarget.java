874868888d3f660275672df97cb5c99669a2dc1e
Georgios Panaretos <georgios.panaretos+SYNPULSE@synpulse.com>
2023-08-27 23:08:51 +0200

    Two Sum Target

--- src/main/java/ArrayManipulation/TwoSumTarget/TwoSumTarget.java
+++ src/main/java/ArrayManipulation/TwoSumTarget/TwoSumTarget.java
@@ -1,4 +1,17 @@
 package ArrayManipulation.TwoSumTarget;
 
 public class TwoSumTarget {
+    public static boolean hasTwoSum(int[] nums, int k) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == k) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
