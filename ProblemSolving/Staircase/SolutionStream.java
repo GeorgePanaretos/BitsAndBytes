d52d9eb49007fa9f95b3a1583ddf5de0628a4628
George Panaretos <georgepanaretos.cac@gmail.com>
2025-01-31 10:20:45 +0100

    Create SolutionStream.java

---
+++ ProblemSolving/Staircase/SolutionStream.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n) {
+       String chara = "#";
+       String empty =" ";
+       List<String> newList = IntStream
+            .rangeClosed(1, n)
+            .mapToObj(i -> empty.repeat(n-i) + chara.repeat(i))
+            .collect(Collectors.toList());
+
+       newList.forEach(System.out::println);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+}
