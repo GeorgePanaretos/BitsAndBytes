ccd77736886ffc889635f190f7d08d3b9e0b4b33
George Panaretos <georgepanaretos.cac@gmail.com>
2025-03-27 17:15:06 +0100

    Robot Vaccum

---
+++ Java/StringManipulation/RobotVacuumReturn/RobotVacuum.java
@@ -0,0 +1,16 @@
+package StringManipulation.RobotVacuumReturn;
+
+public class RobotVacuum {
+    public static boolean isRobotReturnToOrigin(String moves) {
+        int x = 0, y = 0;
+
+        for (char move : moves.toCharArray()) {
+            if (move == 'L') x--;
+            else if (move == 'R') x++;
+            else if (move == 'U') y++;
+            else if (move == 'D') y--;
+        }
+
+        return x == 0 && y == 0;
+    }
+}
